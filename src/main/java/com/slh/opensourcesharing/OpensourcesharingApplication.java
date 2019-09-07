6d530b2306f5c45095b4dad3f07e32d8bb8ab48d
Your Name <you@example.com>
2019-09-07 13:35:27 +0200

    second commit

--- src/main/java/com/slh/opensourcesharing/OpensourcesharingApplication.java
+++ src/main/java/com/slh/opensourcesharing/OpensourcesharingApplication.java
@@ -8,6 +8,7 @@ public class OpensourcesharingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpensourcesharingApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
