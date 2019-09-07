431e1ffd0bcd80565c704b6d9df364923904c5b7
Your Name <you@example.com>
2019-09-07 16:46:12 +0200

    test commit

--- src/main/java/com/slh/opensourcesharing/controller/PublicController.java
+++ src/main/java/com/slh/opensourcesharing/controller/PublicController.java
@@ -1,10 +1,11 @@
 package com.slh.opensourcesharing.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller //Changed from @RestController
 @RequestMapping("/api/public")
 public class PublicController {
 
--- src/main/resources/templates/home.jsp
+++ src/main/resources/templates/home.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html >
+<html xmlns:th="http://www.thymeleaf.org">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Create an account</title>
