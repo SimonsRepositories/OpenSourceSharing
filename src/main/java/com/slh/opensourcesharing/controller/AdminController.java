e283f4061486683b2c66f70a6cb637650d7fefae
Your Name <you@example.com>
2019-09-08 13:35:51 +0200

    configurated configure method

--- src/main/java/com/slh/opensourcesharing/controller/AdminController.java
+++ src/main/java/com/slh/opensourcesharing/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.slh.opensourcesharing.controller;
 import com.slh.opensourcesharing.model.User;
 import com.slh.opensourcesharing.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ public class AdminController
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/admin/add")
     public String addUserBAdmin(@RequestBody User user)
     {
