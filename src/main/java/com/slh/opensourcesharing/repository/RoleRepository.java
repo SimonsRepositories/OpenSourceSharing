0526931684126c6225cec54ccd2024228f1794a5
Your Name <you@example.com>
2019-09-08 11:21:48 +0200

    Added interface repositories Role and User

---
+++ src/main/java/com/slh/opensourcesharing/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.slh.opensourcesharing.repository;
+
+import com.slh.opensourcesharing.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>
+{
+
+}
---
+++ src/main/java/com/slh/opensourcesharing/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.slh.opensourcesharing.repository;
+
+import com.slh.opensourcesharing.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+
+}
