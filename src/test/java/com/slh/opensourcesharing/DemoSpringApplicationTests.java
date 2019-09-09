8abf42eff53eec5cbce28c5b134b703c81c8c1a3
Your Name <you@example.com>
2019-09-09 20:44:09 +0200

    testing something out. (Didn't work)

--- src/test/java/com/slh/opensourcesharing/DemoSpringApplicationTests.java
+++ src/test/java/com/slh/opensourcesharing/DemoSpringApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OpensourcesharingApplicationTests {
+public class DemoSpringApplicationTests {
 
 	@Test
 	public void contextLoads() {
