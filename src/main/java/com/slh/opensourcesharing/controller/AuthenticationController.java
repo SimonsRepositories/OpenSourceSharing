bd38df28156eda627981f80398e2d3304dffc938
SimonsRepositories <simonh@bluewin.ch>
2019-09-09 13:21:06 +0200

    Added Post Method for /register

--- src/main/java/com/slh/opensourcesharing/controller/AuthenticationController.java
+++ src/main/java/com/slh/opensourcesharing/controller/AuthenticationController.java
@@ -1,9 +1,14 @@
 package com.slh.opensourcesharing.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.slh.opensourcesharing.model.User;
+import javax.validation.Valid;
 
 @Controller
 public class AuthenticationController
@@ -28,6 +33,24 @@ public class AuthenticationController
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home"); //resources/templates/home.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/register", method = RequestMethod.POST)
+    public ModelAndView registerUser(@Valid User user, BindingResult bindingResult, ModelMap modelMap) {
+        ModelAndView modelAndView = new ModelAndView();
+        // CHeck for the validation
+        if (bindingResult.hasErrors()) {
+            modelAndView.addObject("successMessage", "Please correct the errors in form!");
+            modelMap.addAttribute("bindingResult", bindingResult);
+        }
+        //save the user if no binding errors
+        else {
+            //save the user registration form
+
+        }
+        modelAndView.addObject("user", new User());
+        modelAndView.setViewName("register");
         return modelAndView;
     }
 }
