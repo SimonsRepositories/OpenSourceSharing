ef895446a7082fd7c62b0cc5010cb3bf35d633d4
SimonsRepositories <simonh@bluewin.ch>
2019-09-09 13:19:28 +0200

    Add status to user class

--- src/main/java/com/slh/opensourcesharing/model/User.java
+++ src/main/java/com/slh/opensourcesharing/model/User.java
@@ -2,8 +2,11 @@ package com.slh.opensourcesharing.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Entity
@@ -13,21 +16,30 @@ import java.util.Set;
 public class User
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name= "auth_user_id")
     private int id;
 
+    @NotNull(message = "first name is compulsory")
     @Column(name = "first_name")
     private String name;
 
+    @NotNull(message = "Last name name is compulsory")
     @Column(name = "last_name")
     private String lastname;
 
+    @NotNull(message = "email name is compulsory")
+    @Email(message ="Email is invalid")
     @Column(name = "email")
     private String email;
 
+    @NotNull(message = "password name is compulsory")
+    @Length(min=5, message = "Password should be at least 5 characters")
     @Column(name = "password")
     private String password;
+
+    @Column(name = "status")
+    private String status;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(name="user_role", joinColumns = @JoinColumn(name="user_id"),
