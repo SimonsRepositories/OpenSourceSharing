0852c109e79cf644ab47a73f461546d6d22af03d
SimonsRepositories <simonh@bluewin.ch>
2019-09-09 11:22:41 +0200

    Controllers deleted due to unexplicable error

--- src/main/java/com/slh/opensourcesharing/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.slh.opensourcesharing.repository;
-
-import com.slh.opensourcesharing.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>
-{
-    //User findByUsername(String username);
-}
