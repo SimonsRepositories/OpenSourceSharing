6befd9615fcda8514db6b9f5aaa362245fe4c061
SimonsRepositories <simonh@bluewin.ch>
2019-09-10 11:10:47 +0200

    Service for Controller with crud methods for post

---
+++ src/main/java/com/slh/opensourcesharing/service/PostList.java
@@ -0,0 +1,61 @@
+package com.slh.opensourcesharing.service;
+
+import com.slh.opensourcesharing.model.Post;
+import org.springframework.stereotype.Service;
+import com.slh.opensourcesharing.model.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PostList
+{
+    public List<Post> posts = new ArrayList<>(Arrays.asList(
+            new Post(1, "Simon Huster", "This is my code"),
+            new Post(2, "Sascha Huber", "tutorial, how to...")
+    ));
+
+    public List<Post> getAllPosts()
+    {
+        if (posts.isEmpty()) {
+            return null;
+        }
+        return posts;
+    }
+
+    public void addPost(Post value) {
+        posts.add(value);
+    }
+
+    public Post getPost(long id) {
+        Post post = null;
+        for(Post postx: posts) {
+            if(post.getId() == id) {
+                post = postx;
+            }
+        }
+        return post;
+    }
+
+    public void removePost(long id)
+    {
+        for(Post post: posts) {
+            if(post.getId() == id) {
+                posts.remove(post);
+                break;
+            }
+        }
+    }
+
+    public void updatePost(long id, Post post)
+    {
+        for(int i = 0; i < posts.size(); i++) {
+            Post tmpValue = posts.get(i);
+            if (tmpValue.getId() == id) {
+                posts.set(i, post);
+                return;
+            }
+        }
+    }
+}
